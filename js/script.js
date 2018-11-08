2787d50f39d3b684bed729e2fad769897bb704d7
Tim Nelson <TravelTimN@gmail.com>
2018-11-08 19:56:21 +0000

    added Leaflet MarkerCluster / custom Airplane Marker with testData.js

--- js/script.js
+++ js/script.js
@@ -25,14 +25,45 @@ $(document).ready(function () {
         attribution: attributes
     });
 
+    // assigning my custom Icons
+    var airplaneIcon = new L.Icon({
+        iconUrl: "images/airplane.png",
+        iconSize: [50, 50],
+        iconAnchor: [25, 50]
+    });
+
+    // adding the data to Popups
+    function addPopupData(feature) {
+        var popupData = "";
+        popupData += feature.properties.iata;
+        return popupData;
+    }
+
+    // creating the Layers
+    var airports = new L.LayerGroup();
+
+    // assigning the data to the respective layer
+    var markers = L.markerClusterGroup();
+    var theMarkers = L.geoJson(testData, {
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(addPopupData(feature));
+            if (feature.properties.type === "airport") {
+                layer.setIcon(airplaneIcon);
+                layer.addTo(airports);
+            }
+        }
+    });
+
     // map Overlay
     var mapOverlay = L.tileLayer("http://services.arcgisonline.com/arcgis/rest/services/Reference/World_Boundaries_and_Places/MapServer/tile/{z}/{y}/{x}");
 
     // the Map
     var map = L.map("map", {
         layers: [osmDarkMap],
-        center: [30.15, 4.53],
-        zoom: 3,
+        //center: [30.15, 4.53], // temporary changing to Dublin for testing icon markers
+        center: [53.345959, -6.273580],
+        //zoom: 3, // temporary changing to Dublin for testing icon markers
+        zoom: 7,
         minZoom: 2,
         maxZoom: 18
     });
@@ -44,10 +75,18 @@ $(document).ready(function () {
         "Earth": arcgisEarthMap
     };
 
+    // map Options
+    var mapOptions = {
+        "Airports": airports
+    };
+
     // add Map Layer Control
-    L.control.layers(mapLayers).addTo(map);
+    L.control.layers(mapLayers, mapOptions).addTo(map);
 
     // add Map Overlay and sit on very top
     mapOverlay.bringToFront().addTo(map).setZIndex(9);
+
+    // add Icons to Map
+    markers.addLayer(theMarkers);
 
 });
---
+++ js/testData.js
@@ -0,0 +1,196 @@
+var testData = {
+    "type": "FeatureCollection",
+    "features": [{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-9.48416996, 51.66859818]
+            },
+            "properties": {
+                "iata": "BYT",
+                "type": "airport",
+                "name": "Bantry Aerodrome",
+                "country": "Ireland",
+                "wikipedia": "https://en.wikipedia.org/wiki/Bantry_Aerodrome"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-8.491109848, 51.84130096]
+            },
+            "properties": {
+                "iata": "ORK",
+                "type": "airport",
+                "name": "Cork Airport",
+                "country": "Ireland",
+                "wikipedia": "https://en.wikipedia.org/wiki/Cork_Airport"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-8.924819946, 52.70199966]
+            },
+            "properties": {
+                "iata": "SNN",
+                "type": "airport",
+                "name": "Shannon Airport",
+                "country": "Ireland",
+                "wikipedia": "https://en.wikipedia.org/wiki/Shannon_Airport"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-6.27007, 53.421299]
+            },
+            "properties": {
+                "iata": "DUB",
+                "type": "airport",
+                "name": "Dublin Airport",
+                "country": "Ireland",
+                "wikipedia": "https://en.wikipedia.org/wiki/Dublin_Airport"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-8.340999603, 55.0442009]
+            },
+            "properties": {
+                "iata": "CFN",
+                "type": "airport",
+                "name": "Donegal Airport",
+                "country": "Ireland",
+                "wikipedia": "https://en.wikipedia.org/wiki/Donegal_Airport"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-7.672830105, 54.95130157]
+            },
+            "properties": {
+                "iata": "LTR",
+                "type": "airport",
+                "name": "Letterkenny Airport",
+                "country": "Ireland"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-8.941590309, 53.30020142]
+            },
+            "properties": {
+                "iata": "GWY",
+                "type": "airport",
+                "name": "Galway Airport",
+                "country": "Ireland",
+                "wikipedia": "https://en.wikipedia.org/wiki/Galway_Airport"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-9.568059921, 53.0929985]
+            },
+            "properties": {
+                "iata": "IIA",
+                "type": "airport",
+                "name": "Inishmaan Aerodrome",
+                "country": "Ireland"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-9.653610229, 53.1067009]
+            },
+            "properties": {
+                "iata": "IOR",
+                "type": "airport",
+                "name": "Inishmore Aerodrome",
+                "country": "Ireland",
+                "wikipedia": "https://en.wikipedia.org/wiki/Inishmore_Aerodrome"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-9.510899544, 53.06470108]
+            },
+            "properties": {
+                "iata": "INQ",
+                "type": "airport",
+                "name": "Inisheer Aerodrome",
+                "country": "Ireland"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-9.467780113, 53.2303009]
+            },
+            "properties": {
+                "iata": "NNR",
+                "type": "airport",
+                "name": "Connemara Regional Airport",
+                "country": "Ireland",
+                "wikipedia": "https://en.wikipedia.org/wiki/Aerfort_na_Minna"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-7.296110153, 52.6507988]
+            },
+            "properties": {
+                "iata": "KKY",
+                "type": "airport",
+                "name": "Kilkenny Airport",
+                "country": "Ireland",
+                "wikipedia": "https://en.wikipedia.org/wiki/Kilkenny_Airport"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-10.23333263, 51.93333435]
+            },
+            "properties": {
+                "iata": "CHE",
+                "type": "airport",
+                "name": "Reeroe Airport",
+                "country": "Ireland"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-9.523779869, 52.18090057]
+            },
+            "properties": {
+                "iata": "KIR",
+                "type": "airport",
+                "name": "Kerry Airport",
+                "country": "Ireland",
+                "wikipedia": "https://en.wikipedia.org/wiki/Kerry_Airport"
+            }
+        }
+    ]
+};
